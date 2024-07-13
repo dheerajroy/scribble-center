489536974d4de9511d06e6ec8b3c96d84d0a5d6e
D Dheeraj <dhiraajroy123@gmail.com>
2024-07-13 17:03:01 +0530

    changed canvas to fixed

--- app/page.jsx
+++ app/page.jsx
@@ -62,7 +62,7 @@ export default function PersistenceExample() {
     }
 
     return (
-        <div className="w-full h-screen">
+        <div className="fixed top-0 left-0 w-full h-screen">
             <Tldraw store={store} theme="dark" />
         </div>
     );
